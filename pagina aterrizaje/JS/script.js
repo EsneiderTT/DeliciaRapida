eab9c65a49f2e4f645c72646894321f771830698
EsneiderTT <esneidertrujillo054@gmail.com>
2026-08-19 07:36:21 -0500

    primera carga

---
+++ pagina aterrizaje/JS/script.js
@@ -0,0 +1,10 @@
+const botonRegistro = document.getElementById("registrarse");
+const botonInicio = document.getElementById("iniciarSesion");
+
+botonRegistro.addEventListener("click", function () {
+    alert("Vamos a registrarte");
+});
+
+botonInicio.addEventListener("click", function () {
+    alert("Vamos a iniciar sesión");
+});
